fa1f3b41ec8497054f824d4dbc55836861bf8cbc
Landon Schropp <schroppl@gmail.com>
2025-09-16 23:49:51 -0700

    Simplify env
    The previous tests for env were interfering with other tests, and
    honestly testing that functionality at all was probably overkill.

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,11 @@
-export function requireEnvironmentVariable(name: string): string {
+/**
+ * Ensures that an environment variable has set.
+ *
+ * @param name The name of the environment variable.
+ * @returns The value of the environment variable.
+ * @throws If the environment variable is not set.
+ */
+function requireEnvironmentVariable(name: string): string {
   const value = process.env[name];
 
   if (!value) {
